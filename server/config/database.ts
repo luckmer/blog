9d0d6611595f798ed327177ceb81d4b2aa0a3a7d
luckmer <woszczo04@gmail.com>
2021-08-23 10:22:31 +0200

    database config update

---
+++ server/config/database.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const CONNECTION_URL = mongoose
+  .connect(CONNECTION_URL, {
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("mongo on");
+  })
+  .catch((err) => console.log(`${err} error`));
