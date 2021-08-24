5c1727682e83854b3e724b0973f1b8556e3b447e
luckmer <woszczo04@gmail.com>
2021-08-24 10:34:12 +0200

    user display panel added validation forms

---
+++ client/src/views/blog/Blog.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const MainBlog = styled.section`
+  height: 100%;
+`;
+
+const Blog = () => {
+  return <MainBlog>blog</MainBlog>;
+};
+
+export default Blog;
