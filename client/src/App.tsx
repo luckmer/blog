763b658688d3956368575cd66bb282f48b55d004
luckmer <woszczo04@gmail.com>
2021-08-25 14:33:26 +0200

    added routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,30 @@
+import styled from "styled-components";
+import { NavBar } from "./components";
+import { Switch, Route, useLocation } from "react-router-dom";
+import { routes } from "./routes";
+import Api from "./redux/services/Api";
+
+const Main = styled.main`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+`;
+
 const App = () => {
-  return <div></div>;
+  const location = useLocation();
+
+  Api();
+
+  return (
+    <Main>
+      <NavBar />
+      <Switch location={location} key={location.pathname}>
+        {routes.map(({ path, Component }) => (
+          <Route key={path} exact path={path} component={Component} />
+        ))}
+      </Switch>
+    </Main>
+  );
 };
 
 export default App;
