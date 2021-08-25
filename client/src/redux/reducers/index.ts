032a589457ddfab08b1e064c5e0ab1a97fb7f622
luckmer <woszczo04@gmail.com>
2021-08-25 14:28:49 +0200

    reducers combine

--- client/src/redux/reducers/index.ts
+++ client/src/redux/reducers/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import UserReducer from "./UserReducer";
+import registerReducer from "./registerReducer";
 
 const Index = combineReducers({
   user: UserReducer,
+  registration: registerReducer,
 });
 
 export default Index;
