d8f9f7abb6e79f34b8a43c2865d7036a6c4b321e
luckmer <woszczo04@gmail.com>
2021-08-25 14:28:37 +0200

    connected saga

--- client/src/redux/store.tsx
+++ client/src/redux/store.tsx
@@ -1,11 +1,18 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import Index from "./reducers/index";
 import createSagaMiddleware from "redux-saga";
+import Saga from "./saga/Index";
+
+const sagaMiddleware = createSagaMiddleware();
+const middleware = [...getDefaultMiddleware({ thunk: false }), sagaMiddleware];
 
 const store = configureStore({
   reducer: {
     back: Index,
   },
+  middleware,
 });
+
+sagaMiddleware.run(Saga);
 
 export default store;
