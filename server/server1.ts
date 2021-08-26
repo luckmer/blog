1550b9f3ab5ad45e15257b5c360e3d2cd7c9499a
luckmer <woszczo04@gmail.com>
2021-08-26 17:08:00 +0200

    server update

--- server/server1.ts
+++ server/server1.ts
@@ -7,14 +7,16 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import morgan from "morgan";
 import routes from "./routes/index";
-
+import blogRoutes from "./routes/blogPost";
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(morgan("dev"));
 app.use(cookieParser());
 app.use("/user", routes);
+app.use("/user", blogRoutes);
 
 const CONNECTION_URL = ``;
 const PORT = process.env.PORT || 5000;
@@ -24,6 +26,7 @@ mongoose
     useUnifiedTopology: true,
     useCreateIndex: true,
     useNewUrlParser: true,
+    useFindAndModify: false,
   })
   .then(() => app.listen(PORT, () => console.log(`connected `)))
   .catch((err) => console.log(`${err} error`));
