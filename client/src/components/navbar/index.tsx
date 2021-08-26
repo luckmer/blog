87ebf417f756056602c047c713ffd40894a0c5ac
luckmer <woszczo04@gmail.com>
2021-08-26 17:09:06 +0200

    added user icon

--- client/src/components/navbar/index.tsx
+++ client/src/components/navbar/index.tsx
@@ -1,7 +1,31 @@
+import { Fragment } from "react";
 import { Link } from "react-router-dom";
 import * as nav from "../../css/Nav.css";
+import { useSelector, TypedUseSelectorHook } from "react-redux";
+
+interface NavBarState {
+  back: {
+    registration: {
+      userStatus: boolean;
+      userData: {
+        email: string;
+        name: string;
+        _id: string;
+      };
+    };
+  };
+}
+
+interface postState {
+  id: string;
+}
 
 const Index = () => {
+  const Typed: TypedUseSelectorHook<NavBarState> = useSelector;
+  const state = Typed((state) => state.back.registration);
+
+  const id = state.userData?._id;
+
   return (
     <nav.Nav>
       <nav.NavContent>
@@ -17,16 +41,7 @@ const Index = () => {
           <nav.Input placeholder="search content" />
         </nav.NavSpacer>
         <nav.NavSpacer>
-          <nav.Ul>
-            <Link to="/login">
-              <li>Login</li>
-            </Link>
-          </nav.Ul>
-          <nav.Ul>
-            <Link to="/register">
-              <li>Register</li>
-            </Link>
-          </nav.Ul>
+          {id ? <PostGenerator id={id} /> : <LoginPanel />}
         </nav.NavSpacer>
       </nav.NavContent>
     </nav.Nav>
@@ -34,3 +49,35 @@ const Index = () => {
 };
 
 export default Index;
+
+const PostGenerator = ({ id }: postState) => {
+  return (
+    <Fragment>
+      <nav.Ul>
+        <Link to="/postCreator">
+          <li>Create Post</li>
+        </Link>
+        <Link to={`/profile/${id}?id=${id}&check=`}>
+          <nav.NAVIMG src=" " alt="" />
+        </Link>
+      </nav.Ul>
+    </Fragment>
+  );
+};
+
+const LoginPanel = () => {
+  return (
+    <Fragment>
+      <nav.Ul>
+        <Link to="/login">
+          <li>Login</li>
+        </Link>
+      </nav.Ul>
+      <nav.Ul>
+        <Link to="/register">
+          <li>Register</li>
+        </Link>
+      </nav.Ul>
+    </Fragment>
+  );
+};
