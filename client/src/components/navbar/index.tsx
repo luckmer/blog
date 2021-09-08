77af2d452443a9427febfe66cc340ba7aa7cebc8
luckmer <woszczo04@gmail.com>
2021-09-08 10:40:06 +0200

    cleared code

--- client/src/components/navbar/index.tsx
+++ client/src/components/navbar/index.tsx
@@ -1,27 +1,9 @@
-import { Fragment } from "react";
 import { Link } from "react-router-dom";
 import * as nav from "../../css/Nav.css";
 import { useSelector, TypedUseSelectorHook } from "react-redux";
-
-interface NavBarState {
-  back: {
-    registration: {
-      avatar: string;
-      userStatus: boolean;
-      userData: {
-        email: string;
-        name: string;
-        _id: string;
-        avatar: string;
-      };
-    };
-  };
-}
-
-interface postState {
-  id: string;
-  avatar: string;
-}
+import { NavBarState } from "../Constants/NavBarState";
+import { PostGenerator } from "./PostGenerator";
+import { LoginPanel } from "./LoginPanel";
 
 const Index = () => {
   const Typed: TypedUseSelectorHook<NavBarState> = useSelector;
@@ -53,35 +35,3 @@ const Index = () => {
 };
 
 export default Index;
-
-const PostGenerator = ({ id, avatar }: postState) => {
-  return (
-    <Fragment>
-      <nav.Ul>
-        <Link to="/postCreator">
-          <li>Create Post</li>
-        </Link>
-        <Link to={`/profile/${id}?id=${id}&check=`}>
-          <nav.NAVIMG src={avatar} alt="" />
-        </Link>
-      </nav.Ul>
-    </Fragment>
-  );
-};
-
-const LoginPanel = () => {
-  return (
-    <Fragment>
-      <nav.Ul>
-        <Link to="/login">
-          <li>Login</li>
-        </Link>
-      </nav.Ul>
-      <nav.Ul>
-        <Link to="/register">
-          <li>Register</li>
-        </Link>
-      </nav.Ul>
-    </Fragment>
-  );
-};
