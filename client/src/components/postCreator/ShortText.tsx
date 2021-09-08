9f39d2df1651b6d1726e37f293cbbc98b3329d9e
luckmer <woszczo04@gmail.com>
2021-09-08 10:44:18 +0200

    text cutter if is too long

---
+++ client/src/components/postCreator/ShortText.tsx
@@ -0,0 +1,30 @@
+import { Fragment, memo } from "react";
+import * as Small from "../../css/PostCreator.css";
+import { ShortState, ErrorObj } from "../Constants/Types";
+
+const ShortText = memo(({ description }: ShortState) => {
+  const maxWidth = 50;
+
+  return (
+    <Fragment>
+      {description.length >= maxWidth
+        ? description.substring(0, maxWidth - 3) + "..."
+        : description}
+    </Fragment>
+  );
+});
+
+const SmallDesc = (
+  category: string,
+  Errors: ErrorObj | undefined,
+  type: string
+) => {
+  const Err = Errors && Errors[type];
+  return (
+    <Small.Small Off={category.length >= 50 || Err ? true : false}>
+      {Err ? Err : `${category.length}/50 `}
+    </Small.Small>
+  );
+};
+
+export { ShortText, SmallDesc };
