939a4a062be4ae508058986f1393fc81b2baba8a
luckmer <woszczo04@gmail.com>
2021-09-08 10:40:39 +0200

    split code into folders

--- client/src/components/postCreator/PostCreator.tsx
+++ client/src/components/postCreator/PostCreator.tsx
@@ -1,5 +1,49 @@
+import { memo, useMemo } from "react";
+import * as Small from "../../css/PostCreator.css";
+import { PostForm, PreviewPost } from ".";
+import Firmware from "./firmware";
+
 const PostCreator = () => {
-  return <div></div>;
+  const firmware = Firmware();
+
+  const MemorizePreviewPost = useMemo(
+    () => (
+      <PreviewPost
+        imgPreview={firmware.imgPreview}
+        header={firmware.header}
+        description={firmware.description}
+      />
+    ),
+    [firmware.header, firmware.imgPreview, firmware.description]
+  );
+
+  return (
+    <Small.Main>
+      <header>
+        <Small.H1>Create Post</Small.H1>
+      </header>
+      <Small.Content>
+        <Small.Spacer>
+          {PostForm(
+            firmware.handleSubmit,
+            firmware.handleChange,
+            firmware.header,
+            firmware.Errors,
+            firmware.ImagePreview,
+            firmware.description,
+            firmware.category,
+            firmware.handleCancel,
+            firmware.TextPreview,
+            firmware.imgError
+          )}
+        </Small.Spacer>
+        <Small.Spacer>
+          <Small.Span>Preview</Small.Span>
+          {MemorizePreviewPost}
+        </Small.Spacer>
+      </Small.Content>
+    </Small.Main>
+  );
 };
 
-export default PostCreator;
+export default memo(PostCreator);
