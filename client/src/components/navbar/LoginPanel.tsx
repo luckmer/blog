eb4de728d174ccb591f797ba5255872e41b86fcd
luckmer <woszczo04@gmail.com>
2021-09-08 10:43:11 +0200

    display if user is not logged in

---
+++ client/src/components/navbar/LoginPanel.tsx
@@ -0,0 +1,20 @@
+import { Fragment } from "react";
+import { Link } from "react-router-dom";
+import * as nav from "../../css/Nav.css";
+
+export const LoginPanel = () => {
+  return (
+    <Fragment>
+      <nav.Ul>
+        <Link to="/login">
+          <li>Login</li>
+        </Link>
+      </nav.Ul>
+      <nav.Ul>
+        <Link to="/register">
+          <li>Register</li>
+        </Link>
+      </nav.Ul>
+    </Fragment>
+  );
+};
