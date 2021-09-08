749e08c27d5e11e6b0d9ac551bef27694e82e1bb
luckmer <woszczo04@gmail.com>
2021-09-08 10:44:04 +0200

    panel to preview post

---
+++ client/src/components/postCreator/PreviewPost.tsx
@@ -0,0 +1,40 @@
+import { useMemo } from "react";
+import * as Small from "../../css/PostCreator.css";
+import { ShortText } from "./ShortText";
+import { PreviewState } from "../Constants/Types";
+
+const PreviewPost = ({ imgPreview, header, description }: PreviewState) => {
+  const shortHeader = useMemo(
+    () => <ShortText description={header} />,
+    [header]
+  );
+
+  const shortDescription = useMemo(
+    () => <ShortText description={description} />,
+    [description]
+  );
+
+  return (
+    <Small.PreviewPanel>
+      <Small.IMGPreview>
+        {typeof imgPreview === "object" ? (
+          <img src={URL.createObjectURL(imgPreview)} alt="" />
+        ) : (
+          " "
+        )}
+        <div>
+          <div>
+            {header ? <Small.P Type="header">{shortHeader}</Small.P> : ""}
+            {description ? (
+              <Small.P Type="description">{shortDescription}</Small.P>
+            ) : (
+              ""
+            )}
+          </div>
+        </div>
+      </Small.IMGPreview>
+    </Small.PreviewPanel>
+  );
+};
+
+export default PreviewPost;
