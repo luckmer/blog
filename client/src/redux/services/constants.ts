3acfcec3164784413f82d81a80b7c453385b23f0
luckmer <woszczo04@gmail.com>
2021-09-09 13:38:05 +0200

    fix problem with multiple data

--- client/src/redux/services/constants.ts
+++ client/src/redux/services/constants.ts
@@ -6,9 +6,10 @@ export const globalPostParameter = (data: Params) => {
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(data.data),
+    body: JSON.stringify(!data.data ? data : data.data),
   };
 };
+
 export const globalPutParameter = (data: DataS | File) => {
   return {
     method: "PUT",
