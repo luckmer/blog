ef89c7e571802fbc5b895b11eb89d320b8505335
luckmer <woszczo04@gmail.com>
2021-09-10 17:46:08 +0200

    added unique category filter

--- client/src/hooks/index.ts
+++ client/src/hooks/index.ts
@@ -1,3 +1,4 @@
 export { default as CreateRefs } from "./CreateRefs";
 export { default as CreateForm } from "./CreateForm";
 export { default as RegistrationForm } from "./RegistrationForm";
+export { default as uniqCategoryFilter } from "./UniqueCateogryFilter";
