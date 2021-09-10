cc246213a64f9c9404cb9de8ff3b2614ec46103f
luckmer <woszczo04@gmail.com>
2021-09-10 17:50:39 +0200

    profile types

---
+++ client/src/components/Types/PropsPosts.types.ts
@@ -0,0 +1,29 @@
+import * as Type from "./FillInterface";
+
+export interface PropsPosts {
+  back: {
+    posts: {
+      posts: {
+        [key: string]: string;
+      }[];
+    };
+  };
+}
+export interface Props {
+  handleChangeImg: (e: Type.InputChange) => Promise<void>;
+  handleLogout: () => void;
+  flag: boolean;
+  fillForm: Type.FillInterface | undefined;
+  user: string;
+  elRefs: HTMLFormElement[];
+  handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+  registerTypes: string[];
+  descriptions: string[];
+  state: {
+    PasswordResult: string;
+  };
+}
+
+export interface dataProp {
+  [key: string]: string;
+}
