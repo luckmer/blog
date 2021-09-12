2005f0444ed9c229cd25fd90becf35302fed4dc1
luckmer <woszczo04@gmail.com>
2021-09-12 19:45:42 +0200

    added new reducer

--- client/src/redux/reducers/index.ts
+++ client/src/redux/reducers/index.ts
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import UserReducer from "./UserReducer";
 import registerReducer from "./registerReducer";
 import PostSlice from "./Posts";
+import PaginationSlice from "./paginationReducer";
 
 const Index = combineReducers({
   registration: registerReducer,
+  pagination: PaginationSlice,
   user: UserReducer,
   posts: PostSlice,
 });
