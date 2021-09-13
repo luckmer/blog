941db29859ae54db1f86ea9fa98d9a69f163cb44
luckmer <woszczo04@gmail.com>
2021-09-13 11:33:56 +0200

    added mobile version

--- client/src/components/navbar/index.tsx
+++ client/src/components/navbar/index.tsx
@@ -1,23 +1,43 @@
-import { Link } from "react-router-dom";
+import { useState, useEffect, Fragment } from "react";
 import { useSelector, TypedUseSelectorHook } from "react-redux";
 import { NavBarState } from "../Types/NavBarState";
 import { PostGenerator } from "./PostGenerator";
 import { LoginPanel } from "./LoginPanel";
+import { Link } from "react-router-dom";
 
 import * as nav from "../../css/Nav.css";
 
 const Index = () => {
+  const [menu, setMenu] = useState(false);
   const Typed: TypedUseSelectorHook<NavBarState> = useSelector;
   const state = Typed((state) => state.back.registration);
   const user = Typed((state) => state.back.registration?.avatar);
 
   const id = state.userData?._id;
 
+  useEffect(() => {
+    document.body.style.overflow = menu ? "hidden" : "";
+  }, [menu]);
+
   return (
-    <nav.Nav>
-      <nav.NavContent>
+    <Fragment>
+      <nav.Menu state={menu} onClick={() => setMenu(!menu)}>
+        <div />
+        <div />
+        <div />
+      </nav.Menu>
+      <nav.MobileNav state={menu}>
+        <nav.Ul>
+          <Link to="">
+            <li>Fox</li>
+            <span> | Blog</span>
+          </Link>
+        </nav.Ul>
+      </nav.MobileNav>
+
+      <nav.Nav state={menu}>
         <nav.NavSpacer>
-          <nav.Ul>
+          <nav.Ul onClick={() => setMenu(false)}>
             <Link to="">
               <li>Fox</li>
               <span> | Blog</span>
@@ -25,17 +45,21 @@ const Index = () => {
           </nav.Ul>
         </nav.NavSpacer>
         <nav.NavSpacer>
-          <nav.Input placeholder="search content" />
+          <nav.Input />
         </nav.NavSpacer>
         <nav.NavSpacer>
           {id ? (
-            <PostGenerator id={id} avatar={user && user ? user : ""} />
+            <PostGenerator
+              id={id}
+              avatar={user && user ? user : ""}
+              MenuControl={() => setMenu(false)}
+            />
           ) : (
             <LoginPanel />
           )}
         </nav.NavSpacer>
-      </nav.NavContent>
-    </nav.Nav>
+      </nav.Nav>
+    </Fragment>
   );
 };
 
